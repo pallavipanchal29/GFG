56d9e5de724ce08eb69983c58418a39c20ce11d9
pallavi panchal <panchal.pallavi29@gmail.com>
2021-08-25 01:10:47 +0530

    Top k frequent elements

--- src/array/easy/TopKFreqElements.java
+++ src/array/easy/TopKFreqElements.java
@@ -1,13 +1,21 @@
 package array.easy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class TopKFreqElements
 {
-    public static void main(String[] args)
-    {
-        int[] arr = new int[]{1,1,1,2,2,3};
-        int[] answer = topK(arr,arr.length);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] line = br.readLine().split(" ");
+        int[] arr = new int[line.length];
+        for(int i = 0;i< line.length;i++)
+        {
+            arr[i] = Integer.parseInt(line[i]);
+        }
+        int[] answer = topK(arr,3);
 
         for(int i = 0;i<answer.length;i++)
             System.out.print(answer[i]+" ");
@@ -15,21 +23,31 @@ public class TopKFreqElements
     public static int[] topK(int[] nums, int k)
     {
         int[] answer = new int[k];
+        int index = 0;
+        for(int i = 0;i<k;i++)
+            answer[i] = 0;
         HashMap<Integer,Integer> map = new HashMap<>();
         for(int i = 0;i<nums.length;i++)
         {
             map.put(nums[i],map.getOrDefault(nums[i],0)+1);
         }
         List<Map.Entry<Integer,Integer>> list = new LinkedList<>(map.entrySet());
-
-        Collections.sort(list, new Comparator<Map.Entry<Integer, Integer>>() {
-            @Override
-            public int compare(Map.Entry<Integer, Integer> o1, Map.Entry<Integer, Integer> o2)
-            {
-                return 0;
-
-            }
+        Collections.sort(list, (o1, o2) -> {
+            int val1 = o1.getValue();
+            int val2 = o2.getValue();
+            if(val1 == val2)
+                return o2.getKey().compareTo(o1.getKey());
+            else
+                return o2.getValue().compareTo(o1.getValue());
         });
-        return null;
+
+        for(int i = 0;i<list.size();i++)
+        {
+            if(k == 0) return answer;
+            var pair = list.get(i);
+            answer[index++] = pair.getKey();
+            k--;
+        }
+        return answer;
     }
 }
