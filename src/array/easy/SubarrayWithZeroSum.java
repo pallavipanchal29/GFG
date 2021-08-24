f0106a1e45b79ff807569fabcd5abc33c0f17cea
pallavi panchal <panchal.pallavi29@gmail.com>
2021-08-24 20:23:47 +0530

    Subarray with zero sum

--- src/array/easy/SubarrayWithZeroSum.java
+++ src/array/easy/SubarrayWithZeroSum.java
@@ -10,25 +10,7 @@ public class SubarrayWithZeroSum
         int[] arr = new int[]{10,-10};
         System.out.println(findsum(arr,arr.length));
     }
-//    static boolean findsum(int[] arr, int n)
-//    {
-//        int curr_sum = arr[0], start = 0, i;
-//
-//        for (i = 1; i <= n; i++) {
-//            while (curr_sum > 0 && start < i - 1) {
-//                curr_sum = curr_sum - arr[start];
-//                start++;
-//            }
-//
-//            if (curr_sum == 0) {
-//               return true;
-//            }
-//
-//            if (i < n)
-//                curr_sum = curr_sum + arr[i];
-//        }
-//        return false;
-//    }
+
     static boolean findsum(int[] arr, int n)
     {
         int sum = 0;
