1019fc2de6b7f9102746367ce7a8d467a66b6d61
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-02 21:55:55 +0530

    Longest k unique substring

--- src/string/easy/LongestKUniqueSubstring.java
+++ src/string/easy/LongestKUniqueSubstring.java
@@ -1,16 +1,45 @@
 package string.easy;
 
-public class LongestKUniqueSubstring
-{
-    public static void main(String[] args)
-    {
-        System.out.println(longestkSubstr("aabacbebebe",3));
-    }
-    public static int longestkSubstr(String s, int k)
-    {
-       int ans = -1;
-       int i = 0,j = 0;
+import java.util.HashMap;
 
-       return 0;
+public class LongestKUniqueSubstring {
+    public static void main(String[] args) {
+        System.out.println(longestkSubstr("aabacbebebe", 3));
+    }
+
+    public static int longestkSubstr(String s, int k) {
+        int lo = 0;
+        int hi = 0;
+        HashMap<Character, Integer> hm = new HashMap<>();
+        int max = -1;
+        while (hi < s.length())
+        {
+            char ch = ' ';
+            ch = s.charAt(hi);
+            hm.put(ch, hm.getOrDefault(ch, 0) + 1);
+            if (hm.size() < k)
+            {
+                hi++;
+            } else if (hm.size() == k)
+            {
+                max = Math.max(max, hi - lo + 1);
+                hi++;
+            }
+            else if (hm.size() >= k)
+            {
+                while (hm.size() > k)
+                {
+                    char lch = s.charAt(lo);
+                    hm.put(lch, hm.get(lch) - 1);
+                    if (hm.get(lch) == 0)
+                    {
+                        hm.remove(lch);
+                    }
+                    lo++;
+                }
+                hi++;
+            }
+        }
+        return max;
     }
 }
