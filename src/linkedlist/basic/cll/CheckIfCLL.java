f23f64fc53af1571390a190cafed8df30fcf7d6c
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-03 10:35:32 +0530

    Check if circular LL

---
+++ src/linkedlist/basic/cll/CheckIfCLL.java
@@ -0,0 +1,33 @@
+package linkedlist.basic.cll;
+
+import linkedlist.basic.Node;
+
+//Completed GFG - correct
+public class CheckIfCLL
+{
+    static Node head = null;
+    public static void main(String[] args)
+    {
+        head = new Node(5);
+        head.next = new Node(9);
+        head.next.next = new Node(6);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(4);
+        head.next.next.next.next.next = head;
+
+        System.out.println(isCircular(head));
+
+    }
+    static boolean isCircular(Node head)
+    {
+        Node temp = head;
+
+        while (temp != null)
+        {
+            if(temp.next == head)
+                return true;
+            temp = temp.next;
+        }
+        return false;
+    }
+}
