c8eb28a61e2137a89830f1cde392c9756be243c0
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-03 18:33:32 +0530

    StringNode

---
+++ src/linkedlist/easy/LL/StringNode.java
@@ -0,0 +1,13 @@
+package linkedlist.easy.LL;
+
+public class StringNode
+{
+    public char data;
+    public StringNode next;
+
+    public StringNode(char data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
