00db8ca653d6b138acbd6b255ce2ea2ffe329fa1
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-03 10:40:17 +0530

    Node at given index

---
+++ src/linkedlist/basic/NodeAtGivenIndexLL.java
@@ -0,0 +1,33 @@
+package linkedlist.basic;
+
+//Completed GFG - correct
+public class NodeAtGivenIndexLL
+{
+    static Node head = null;
+    public static void main(String[] args)
+    {
+        head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(657);
+        head.next.next.next.next.next = new Node(76);
+
+        System.out.println(getNth(head,5));
+
+    }
+    public static int getNth(Node node, int ind)
+    {
+        Node temp = node;
+        int i = 0;
+
+        while (temp != null && i < ind-1)
+        {
+            temp = temp.next;
+            i++;
+        }
+        if(temp != null)
+            return temp.data;
+        return -1;
+    }
+}
