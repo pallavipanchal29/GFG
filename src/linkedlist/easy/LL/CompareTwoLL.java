8b7aa92942f6705514db791c7461058bdf8bf047
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-03 18:32:36 +0530

    Compare two LL

--- src/linkedlist/easy/LL/CompareTwoLL.java
+++ src/linkedlist/easy/LL/CompareTwoLL.java
@@ -10,41 +10,36 @@ public class CompareTwoLL
 
     public static void main(String[] args)
     {
-        head1 = new StringNode("a");
-        head1.next = new StringNode("a");
-        head1.next.next = new StringNode("a");
-        head1.next.next.next = new StringNode("b");
-        head1.next.next.next.next = new StringNode("a");
+        head1 = new StringNode('a');
+        head1.next = new StringNode('a');
+        head1.next.next = new StringNode('a');
+        head1.next.next.next = new StringNode('b');
+        head1.next.next.next.next = new StringNode('a');
 
-        head2 = new StringNode("a");
-        head2.next = new StringNode("a");
-        head2.next.next = new StringNode("a");
-        head2.next.next.next = new StringNode("b");
-        head2.next.next.next.next = new StringNode("a");
+        head2 = new StringNode('a');
+        head2.next = new StringNode('a');
+        head2.next.next = new StringNode('a');
+        head2.next.next.next = new StringNode('b');
+        head2.next.next.next.next = new StringNode('a');
 
         System.out.println(compare(head1,head2));
     }
     static int compare(StringNode node1, StringNode node2)
     {
-        StringBuilder str1 = new StringBuilder();
-        StringBuilder str2 = new StringBuilder();
-
-        if(node1 == null && node2 == null)
-            return 1;
-        if(node1 == null || node2 == null)
-            return 0;
-
-        while (node1 != null)
+        while (node1 != null && node2 != null)
         {
-            str1.append(node1.data);
+            if(node1.data > node2.data)
+                return 1;
+            else if(node1.data < node2.data)
+                return -1;
             node1 = node1.next;
-        }
-        while (node2 != null)
-        {
-            str2.append(node2.data);
             node2 = node2.next;
-        }
 
-        return str1.toString().compareTo(str2.toString());
+        }
+        if(node1 != null)
+            return 1;
+        if(node2 != null)
+            return -1;
+        return 0;
     }
 }
