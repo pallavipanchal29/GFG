3122806929cf30e0ce1b129cefa6543dd9dbdf0e
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-17 18:01:10 +0530

    Max triplet sum in array

--- src/array/basic/MaximumTripletSumArray.java
+++ src/array/basic/MaximumTripletSumArray.java
@@ -1,5 +1,6 @@
 package array.basic;
 
+//Completed GFG - correct
 public class MaximumTripletSumArray
 {
     public static void main(String[] args)
@@ -21,12 +22,12 @@ public class MaximumTripletSumArray
                 max2 = max1;
                 max1 = a[i];
             }
-            else if(a[i] < max1 && a[i] > max2)
+            else if(a[i] <= max1 && a[i] > max2)
             {
                 max3 = max2;
                 max2 = a[i];
             }
-            else if(a[i] < max2 && a[i] > max3)
+            else if(a[i] <= max2 && a[i] > max3)
                 max3 = a[i];
         }
         return max1 + max2 + max3;
