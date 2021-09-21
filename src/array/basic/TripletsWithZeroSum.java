41b4c4f988f7f9fda82d48b7d810fc02f46dc89c
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-21 23:34:13 +0530

    Triplets with zero sum - hashmap method

--- src/array/basic/TripletsWithZeroSum.java
+++ src/array/basic/TripletsWithZeroSum.java
@@ -1,14 +1,17 @@
 package array.basic;
 
 import java.util.Arrays;
+import java.util.HashSet;
 
 public class TripletsWithZeroSum
 {
     public static void main(String[] args)
     {
-        int[] arr = new int[]{1, 2, 3};
+        int[] arr = new int[]{0, -1, 2, -3, 1};
         System.out.println(findTriplets(arr,arr.length));
     }
+
+    // O(NLogn) complexity O(1) space
     public static boolean findTriplets(int arr[] , int n)
     {
         Arrays.sort(arr);
@@ -28,4 +31,22 @@ public class TripletsWithZeroSum
         }
         return false;
     }
+
+    // O(nsquare) complexity O(n) space
+//    public static boolean findTriplets(int arr[] , int n)
+//    {
+//        for(int i = 0;i<n;i++)
+//        {
+//            HashSet<Integer> set = new HashSet();
+//            for(int j = i+1;j<n;j++)
+//            {
+//                int val = -(arr[i] + arr[j]);
+//                if(set.contains(val))
+//                    return true;
+//                else
+//                    set.add(arr[j]);
+//            }
+//        }
+//        return false;
+//    }
 }
