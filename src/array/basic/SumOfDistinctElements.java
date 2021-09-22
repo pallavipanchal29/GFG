4c4c5f92c6100933572105e09e81df55f7e4abe7
pallavi panchal <panchal.pallavi29@gmail.com>
2021-09-22 10:43:25 +0530

    Optimization - O(1) method added

--- src/array/basic/SumOfDistinctElements.java
+++ src/array/basic/SumOfDistinctElements.java
@@ -7,19 +7,35 @@ public class SumOfDistinctElements
 {
     public static void main(String[] args)
     {
-        int[] arr = new int[]{1, 2, 3, 4, 5};
+        int[] arr = new int[]{5, 1, 2, 4, 6, 7, 3, 6, 7};
         System.out.println(findSum(arr,arr.length));
     }
+      //O(n) space and O(n) time complexity
+//    static int findSum(int arr[], int n)
+//    {
+//       int sum = 0;
+//       HashSet<Integer> set = new HashSet<>();
+//       for(int i = 0;i<n;i++)
+//       {
+//           if(!set.contains(arr[i]))
+//               sum = sum + arr[i];
+//           set.add(arr[i]);
+//       }
+//       return sum;
+//    }
+
+    //O(1) space & O(n) complexity solution
     static int findSum(int arr[], int n)
     {
-       int sum = 0;
-       HashSet<Integer> set = new HashSet<>();
-       for(int i = 0;i<n;i++)
-       {
-           if(!set.contains(arr[i]))
-               sum = sum + arr[i];
-           set.add(arr[i]);
-       }
-       return sum;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+
+            if (arr[Math.abs(arr[i]) - 1] >= 0) {
+                sum += Math.abs(arr[i]);
+                arr[Math.abs(arr[i]) - 1] *= -1;
+            }
+        }
+
+        return sum;
     }
 }
