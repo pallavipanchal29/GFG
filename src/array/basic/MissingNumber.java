006759bee3dd4a7dd6500d506d48fce94e02ceeb
pallavi panchal <panchal.pallavi29@gmail.com>
2021-10-09 09:42:23 +0530

    Missing Number

--- src/array/basic/MissingNumber.java
+++ src/array/basic/MissingNumber.java
@@ -1,23 +1,20 @@
 package array.basic;
 
+//Completed GFG - correct
 public class MissingNumber
 {
     public static void main(String[] args)
     {
         int[] arr = new int[]{2, 5, 3, 1};
-        System.out.println(missingNumber(arr,arr.length));
+        System.out.println(missingNumber(arr,5));
     }
     public static int missingNumber(int a[], int n)
     {
-        int x1 = a[0];
-        int x2 = 1;
+       int expectedSum = n * (n+1)/2;
+       int actualSum = 0;
+       for(int i = 0;i<a.length;i++)
+           actualSum = actualSum + a[i];
 
-        for (int i = 1; i < n; i++)
-            x1 = x1 ^ a[i];
-
-        for (int i = 2; i <= n + 1; i++)
-            x2 = x2 ^ i;
-
-        return (x1 ^ x2);
+       return expectedSum - actualSum;
     }
 }
