e9d58d9401ec52ee963c46030d08b2b5bd7d1bfe
pallavi panchal <panchal.pallavi29@gmail.com>
2021-10-10 12:29:12 +0530

    Error corrected

--- src/array/basic/MinimumDistanceTwoElements.java
+++ src/array/basic/MinimumDistanceTwoElements.java
@@ -1,16 +1,17 @@
 package array.basic;
 
+//Completed GFG - correct
 public class MinimumDistanceTwoElements
 {
     public static void main(String[] args)
     {
-        int[] arr = new int[]{1,2,3,2};
-        System.out.println(minDist(arr,arr.length,1,2));
+        int[] arr = new int[]{86,39,90,67,84,66,62};
+        System.out.println(minDist(arr,arr.length,42,12));
     }
     static int minDist(int a[], int n, int x, int y)
     {
         int minDist = Integer.MAX_VALUE;
-       int p = -1;
+        int p = -1;
 
         for(int i = 0;i<n;i++)
         {
@@ -21,6 +22,6 @@ public class MinimumDistanceTwoElements
                 p = i;
             }
         }
-        return minDist;
+        return minDist == Integer.MAX_VALUE ? -1 : minDist;
     }
 }
