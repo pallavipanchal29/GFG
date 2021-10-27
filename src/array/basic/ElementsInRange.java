34b5b2758ee2976a2589fbd8fa04cb988aa82576
pallavi panchal <panchal.pallavi29@gmail.com>
2021-10-27 08:40:23 +0530

    Elements in range

--- src/array/basic/ElementsInRange.java
+++ src/array/basic/ElementsInRange.java
@@ -1,23 +1,33 @@
 package array.basic;
 
-import java.util.Arrays;
-
+//Completed GFG - correct
 public class ElementsInRange
 {
     public static void main(String[] args)
     {
         long[] arr = new long[]{1, 4, 5, 2, 7, 8, 3};
-        System.out.println(check_elements(arr,arr.length,2,5));
+        System.out.println(check_elements(arr,arr.length,2,6));
     }
     static boolean check_elements(long arr[], int n, int A, int B)
     {
+        int totalElements = (B- A + 1);
+        if(totalElements > n)
+            return false;
+        int count = 0;
+        int[] rangeElems = new int[B+1];
         for(int i = A;i<=B;i++)
+            rangeElems[i] = 1;
+        for(int i = 0;i<n;i++)
         {
-            int res = Arrays.binarySearch(arr,0,n,i);
-//            if(Arrays.binarySearch(arr,0,n,i) < 0)
-//                return false;
-            int a = 4;
+            if(arr[i]>=A && arr[i]<=B)
+            {
+                if(rangeElems[(int) arr[i]] == 1)
+                {
+                    rangeElems[(int)arr[i]]++;
+                    count++;
+                }
+            }
         }
-        return true;
+        return count == totalElements;
     }
 }
