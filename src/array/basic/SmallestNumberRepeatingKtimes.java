341d8dfb5a331102ab9e5939ca9184f6581edd2b
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-11 10:18:49 +0530

    treemap method added

--- src/array/basic/SmallestNumberRepeatingKtimes.java
+++ src/array/basic/SmallestNumberRepeatingKtimes.java
@@ -2,6 +2,7 @@ package array.basic;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.TreeMap;
 
 //Completed CFG - correct
 public class SmallestNumberRepeatingKtimes
@@ -9,7 +10,7 @@ public class SmallestNumberRepeatingKtimes
     public static void main(String[] args)
     {
         int[] arr = new int[]{2, 2 ,1 ,3, 1};
-        System.out.println(findDuplicate(arr,arr.length,2));
+        System.out.println(findDuplicateTreemap(arr,arr.length,2));
     }
     static int findDuplicate(int arr[], int N,int K)
     {
@@ -27,5 +28,21 @@ public class SmallestNumberRepeatingKtimes
             }
         }
         return smallest;
+    }
+    static int findDuplicateTreemap(int arr[], int N,int K)
+    {
+        TreeMap<Integer,Integer> map = new TreeMap<>();
+        for(int i = 0;i<N;i++)
+        {
+            map.put(arr[i],map.getOrDefault(arr[i],0)+1);
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet())
+        {
+            if(entry.getValue() == K)
+            {
+                return entry.getKey();
+            }
+        }
+        return -1;
     }
 }
