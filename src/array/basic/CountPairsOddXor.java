f1cb8ddb2964a00adcac28c29560f4a8f45e00f3
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-11 16:34:03 +0530

    Count pairs with odd xor

---
+++ src/array/basic/CountPairsOddXor.java
@@ -0,0 +1,24 @@
+package array.basic;
+
+public class CountPairsOddXor
+{
+    public static void main(String[] args)
+    {
+        long[] A = new long[]{1, 2, 3};
+        System.out.println(countXorPair(A,A.length));
+    }
+    static long countXorPair(long arr[], int n)
+    {
+        int odd = 0;
+        int even = 0;
+
+        for(int i = 0;i<n;i++)
+        {
+            if(arr[i] % 2 == 0)
+                even++;
+            else
+                odd++;
+        }
+        return odd * even;
+    }
+}
