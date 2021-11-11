5232a21e1d7e43b0ce0647784e4091c6bccf9c98
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-11 15:15:48 +0530

    O(1) space complexity way aded

--- src/array/basic/AbsoluteDistinctCount.java
+++ src/array/basic/AbsoluteDistinctCount.java
@@ -1,26 +1,60 @@
 package array.basic;
 
-import java.util.HashSet;
-
 //Completed GFG - correct
 public class AbsoluteDistinctCount
 {
     public static void main(String[] args)
     {
-        int[] arr = new int[]{-1, -1, -1, -1, 0, 1, 1, 1, 1};
+        int[] arr = new int[]{-3, -2, 0, 3, 4, 5};
         System.out.println(distinctCount(arr,arr.length));
     }
+//    static int distinctCount(int[] arr, int n)
+//    {
+//        for(int i = 0;i<n;i++)
+//        {
+//            if(arr[i] < 0)
+//                arr[i] = arr[i] * -1;
+//        }
+//        HashSet<Integer> set = new HashSet<>();
+//        for(int i = 0;i<n;i++)
+//            set.add(arr[i]);
+//
+//        return set.size();
+//    }
+
+    //Right way as asked in question
     static int distinctCount(int[] arr, int n)
     {
-        for(int i = 0;i<n;i++)
-        {
-            if(arr[i] < 0)
-                arr[i] = arr[i] * -1;
-        }
-        HashSet<Integer> set = new HashSet<>();
-        for(int i = 0;i<n;i++)
-            set.add(arr[i]);
+        int count = n;
 
-        return set.size();
+        int i = 0;
+        int j = n-1;
+        int sum = 0;
+
+        while (i<j)
+        {
+            if(i!= j && arr[i] == arr[i+1])
+            {
+                count--;
+                i++;
+            }
+            if(i!= j && arr[j] == arr[j-1])
+            {
+                count--;
+                j--;
+            }
+            sum = arr[i] + arr[j];
+            if(sum == 0)
+            {
+                count--;
+                i++;
+                j--;
+            }
+            else if(sum <0)
+                i++;
+            else
+                j--;
+        }
+        return count;
     }
 }
