517d2b89e5946efdd2a8c78d3ec0efcec5f22151
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-11 19:13:54 +0530

    Simplified

--- src/string/school/ConvertListCharsString.java
+++ src/string/school/ConvertListCharsString.java
@@ -9,9 +9,6 @@ public class ConvertListCharsString
     }
     public static String chartostr(char arr[], int N)
     {
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0;i<N;i++)
-            sb.append(arr[i]);
-        return sb.toString();
+        return String.valueOf(arr);
     }
 }
