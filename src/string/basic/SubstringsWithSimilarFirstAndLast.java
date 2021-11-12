05c59334b50d3bf4d553e2f058b8f57814f8e970
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-12 21:59:07 +0530

    O(n) method added

--- src/string/basic/SubstringsWithSimilarFirstAndLast.java
+++ src/string/basic/SubstringsWithSimilarFirstAndLast.java
@@ -7,16 +7,39 @@ public class SubstringsWithSimilarFirstAndLast
     {
         System.out.println(countSubstringWithEqualEnds("abcab"));
     }
+
+    //O(nsquare) Complexity
+//    static int countSubstringWithEqualEnds(String s)
+//    {
+//        int result = 0;
+//        int n = s.length();
+//
+//        for (int i = 0; i < n; i++)
+//            for (int j = i; j < n; j++)
+//                if (s.charAt(i) == s.charAt(j))
+//                    result++;
+//
+//        return result;
+//    }
+
+    //O(n) time complexity
     static int countSubstringWithEqualEnds(String s)
     {
         int result = 0;
         int n = s.length();
 
-        for (int i = 0; i < n; i++)
-            for (int j = i; j < n; j++)
-                if (s.charAt(i) == s.charAt(j))
-                    result++;
-
+        int[] count = new int[26];
+        // calculating frequency of each character
+        for (int i=0; i<n; i++) {
+            count[s.charAt(i)-'a']++;
+        }
+        // Computing result using the frequency table
+        for (int i=0; i<26; i++) {
+            // using the formula that we built
+            result += (count[i]*(count[i]+1)/2);
+        }
+        // returning the result
         return result;
     }
+
 }
