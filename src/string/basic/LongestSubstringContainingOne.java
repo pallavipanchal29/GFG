983e78d430f6424d0353236899a534ce28771692
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-12 21:21:37 +0530

    Longest substring containing one

---
+++ src/string/basic/LongestSubstringContainingOne.java
@@ -0,0 +1,30 @@
+package string.basic;
+
+//Completed GFG - correct
+public class LongestSubstringContainingOne
+{
+    public static void main(String[] args)
+    {
+        System.out.println(count("11101110"));
+    }
+
+    public static int count(String s)
+    {
+        int maxLen = Integer.MIN_VALUE;
+        int len = 0;
+
+        for(int i = 0;i<s.length();i++)
+        {
+            if(s.charAt(i) == '1')
+            {
+                len = 1;
+                for(int j = i+1;j<s.length() && s.charAt(j) == '1';j++)
+                {
+                    len++;
+                }
+                maxLen = Math.max(len,maxLen);
+            }
+        }
+        return maxLen;
+    }
+}
