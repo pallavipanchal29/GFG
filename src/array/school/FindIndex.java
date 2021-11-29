b5b6d6efea91876479bd755089e0fcf8ddf637a6
pallavi panchal <panchal.pallavi29@gmail.com>
2021-11-30 00:52:45 +0530

    Optimized method added

--- src/array/school/FindIndex.java
+++ src/array/school/FindIndex.java
@@ -1,42 +1,25 @@
 package array.school;
 
 //Completed GFG - correct
-public class FindIndex
-{
-    public static void main(String[] args)
-    {
-        int[] arr = new int[]{6, 5, 4, 3, 1, 2};
-        int[] answers = findIndex(arr,arr.length,4);
+public class FindIndex {
+    public static void main(String[] args) {
+        int[] arr = new int[]{21, 11, 30, 17, 30, 18, 13, 23, 10, 30, 11, 15, 21, 13, 13, 24, 25, 11, 15, 30, 15, 18, 13};
+        int[] answers = findIndex(arr, arr.length, 899);
 
-        System.out.println(answers[0]+" "+answers[1]);
+        System.out.println(answers[0] + " " + answers[1]);
     }
+
     static int[] findIndex(int a[], int N, int key)
     {
-        int[] answers = new int[2];
-        int left = 0;
-        int right = N-1;
+        if (N == 0)
+            return new int[]{-1, -1};
+        int i = 0;
+        int j = N - 1;
+        while (i < N && a[i] != key)
+            i++;
+        while (j >= 0 && a[j] != key)
+            j--;
 
-        while (left <N)
-        {
-            if(a[left] == key)
-            {
-                answers[0] = left;
-                break;
-            }
-            else
-                left++;
-        }
-
-        while (right>=0)
-        {
-            if(a[right] == key)
-            {
-                answers[1] = right;
-                break;
-            }
-            else
-                right--;
-        }
-        return answers;
+        return i <= j ? new int[]{i, j} : new int[]{-1, -1};
     }
 }
